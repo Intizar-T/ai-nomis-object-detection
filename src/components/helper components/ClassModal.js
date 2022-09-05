522b63caff2eb933255c5a42b2f0bf128d4b9f9f
Intizar-T <intizar.tashov22@gmail.com>
2022-09-05 20:11:13 +0900

    decreased the scraping number to 10

--- src/components/helper components/ClassModal.js
+++ src/components/helper components/ClassModal.js
@@ -1,70 +1,66 @@
 import { useState } from "react";
 import { Dialog } from "@mui/material";
-import { 
-	DialogBody, 
-	DialogFooter, 
-	DialogHeader, 
-	Button, 
-	Input, } from "@material-tailwind/react";
-
+import {
+  DialogBody,
+  DialogFooter,
+  DialogHeader,
+  Button,
+  Input,
+} from "@material-tailwind/react";
 
 export default function ClassModal(props) {
-	const [value, setValue] = useState(1);
-	const [keyword, setKeyword] = useState("");
-	
-	return (
-		<Dialog open={true}>
-			<DialogHeader>
-				Enter image keyword:
-			</DialogHeader>
-			<DialogBody>
-				<Input
-					variant="outlined"
-					type="string"
-					value={keyword}
-					onChange={(e) => {
-						setKeyword(e.target.value);
-					}}
-				/>
-			</DialogBody>
-			<DialogHeader>
-				Enter image count:
-			</DialogHeader>
-			<DialogBody>
-				<Input
-					variant="outlined"
-					type="number"
-					min={1}
-					max={143}
-					value={value}
-					onChange={(e) => {
-						setValue(e.target.valueAsNumber);
-					}}
-				/>
-			</DialogBody>
-			<DialogFooter>
-				<Button
-					onClick={() => {
-						if (value > 0 && value <= 100 && keyword !== "") {
-							props.dispatch({ type: "INIT_LABELS", labels: [keyword] });
-							props.dispatch({ type: "SET_LABELPROMPT", label: false });
-							props.dispatch({ type: "SET_KEYWORD", keyword: keyword });
-							props.dispatch({ type: "SET_COUNT", count: value });
-							props.onConnect();
-						} else if (value <= 0) {
-							setValue(0);
-							alert("PLease enter valid number");
-						} else if (keyword === "") {
-							alert("Keyword cannot be empty")
-						} else {
-							setValue(0);
-							alert("Value is too big");
-						}
-					}}
-				>
-					OK
-				</Button>
-			</DialogFooter>
-		</Dialog>
-	);
+  const [value, setValue] = useState(1);
+  const [keyword, setKeyword] = useState("");
+
+  return (
+    <Dialog open={true}>
+      <DialogHeader>Enter image keyword:</DialogHeader>
+      <DialogBody>
+        <Input
+          variant="outlined"
+          type="string"
+          value={keyword}
+          onChange={(e) => {
+            setKeyword(e.target.value);
+          }}
+        />
+      </DialogBody>
+      <DialogHeader>Enter image count:</DialogHeader>
+      <DialogBody>
+        <Input
+          variant="outlined"
+          type="number"
+          min={1}
+          max={10}
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.valueAsNumber);
+          }}
+        />
+      </DialogBody>
+      <DialogFooter>
+        <Button
+          onClick={() => {
+            if (value > 0 && value <= 10 && keyword !== "") {
+              props.dispatch({ type: "INIT_LABELS", labels: [keyword] });
+              props.dispatch({ type: "SET_LABELPROMPT", label: false });
+              props.dispatch({ type: "SET_KEYWORD", keyword: keyword });
+              props.dispatch({ type: "SET_COUNT", count: value });
+              props.onConnect();
+            } else if (value <= 0) {
+              setValue(0);
+              alert("PLease enter valid number");
+            } else if (keyword === "") {
+              alert("Keyword cannot be empty");
+            } else {
+              setValue(0);
+              alert("Value is too big. Please limit the number to 10 or less!");
+            }
+          }}
+        >
+          OK
+        </Button>
+      </DialogFooter>
+    </Dialog>
+  );
 }
